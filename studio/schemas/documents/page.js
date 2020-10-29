6df033b70d2fbad6bfeda40e1e933537741d7f3a
PreyBird <Eagle@local.tn>
2020-10-29 14:33:53 +0100

    new page

---
+++ studio/schemas/documents/page.js
@@ -0,0 +1,20 @@
+export default{
+    name: 'page',
+    type:'document',
+    title:'Page',
+    ffields:[
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          
+        },
+        {
+          name: 'slug',
+          type: 'slug',
+          title: 'Slug',
+          
+           
+        }
+        ]
+}
